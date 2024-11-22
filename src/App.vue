8d7831b0ef7c653e3cf85f16abd738c72981b165
Juan Astonitas <111322187+NeoRise456@users.noreply.github.com>
2024-11-22 05:14:34 -0500

    feat: add video about the team sprint 4

--- src/App.vue
+++ src/App.vue
@@ -274,7 +274,7 @@ export default defineComponent({
 
         <div class="column">
           <iframe width="640" height="360"
-                  src="https://youtube.com/embed/vuFem8-f_U0">
+                  src="https://youtu.be/fbkvwc38wek">
           </iframe>
           <h1 style="font-size: 40px; font-weight: bold"> {{ $t('product.team') }} </h1>
         </div>
